725f536158cb8c516437d887542e82e9b92076fc
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-03-16 00:00:26 +0900

    Add files via upload

--- C/0x02/0x02_0.c
+++ C/0x02/0x02_0.c
@@ -21,3 +21,46 @@ int main()
     printf("\n");
     return 0;
 }
+
+
+// C++ (코테.ver)
+// #include <iostream>
+
+// using namespace std;
+
+// int n,x,a;
+
+// int main()
+// {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cin >> n >> x;
+//     for(int i=0; i < n; i++)
+//     {
+//         cin >> a;
+//         if(a < x) cout << a << ' '; 
+//     }
+//     cout << '\n';
+// }
+
+// C++ (개발.ver)
+// #include <iostream>
+// int main()
+// {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     int n,x;
+//     int* a = new int[n];
+// 
+//     std::cin >> n >> x;
+//     for(int i=0; i < n; i++)
+//     {
+//         std::cin >> a[i];
+//     }
+//     for(int i=0; i < n; i++)
+//     {
+//         if(a[i] < x) std::cout << a[i] << ' ';
+//     }
+//     delete[] a;
+// }
