0088d0792416b069b5fe500716e01bb3d085d875
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-03-21 23:11:54 +0900

    Add files via upload

---
+++ C++/0x04/0x04_03.cpp
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/1158
+// 1158¹ø. ¿ä¼¼Çª½º
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int MX = 5001, cursor = 0;
+    int dat[MX];
+    int pre[MX], nxt[MX];
+
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
+
+    int num, cnt;
+    cin >> num, cnt;
+
+    for(int i=0; i < num ;i++)
+    {
+        dat[i] = 
+    }
+
+    cout << "\n";
+}
