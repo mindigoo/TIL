c455874aafc2eade00695b278c1eaf36a949cec8
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-03-22 23:06:28 +0900

    Add files via upload

---
+++ C++/0x05/0x05_04.cpp
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/2493
+// 2493번. 탑
+
+// 다시 풀어보기
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    stack<int> s;
+    int cnt, data=1;
+
+}
+
