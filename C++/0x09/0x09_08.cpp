c3f7b02eb0f4fd2d175b934050c5aff3f45846f9
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-03-31 03:00:44 +0900

    Add files via upload

--- C++/0x09/0x09_08.cpp
+++ C++/0x09/0x09_08.cpp
@@ -1,5 +1,5 @@
-// https://www.acmicpc.net/problem/5427
-// 5427번. 불
+// https://www.acmicpc.net/problem/5014
+// 5014번. 스타트링크
 
 #include<bits/stdc++.h>
 
@@ -8,16 +8,36 @@
 
 using namespace std;
 
-int board[1005][1005];
-int vit[1005][1005];
-
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int vit[1000000];
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int F, S, G, U, D;
+    cin >> F >> S >> G >> U >> D;
 
+    queue<int> Q;
+    int result = 0;
+
+    Q.push(S);
+    vit[S] = 1;
+
+    while(!Q.empty()){
+        int pos = Q.front(); Q.pop();
+        if(pos == G){
+            cout << vit[pos]-1 << "\n";
+            result = 1;
+            break;
+        }
+        for(int move : {pos + U, pos - D}){
+            if(move < 1 || move > F) continue;
+            if(vit[move] > 0) continue;
+
+            vit[move] = vit[pos]+1;
+            Q.push({move});
+        }
+    }
+    if(!result) cout << "use the stairs\n";
 }
