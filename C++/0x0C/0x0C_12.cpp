c82456087a772034c4cceacc74aedc451c237f8a
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-04-07 03:37:19 +0900

    Add files via upload

---
+++ C++/0x0C/0x0C_12.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/6603
+// 6603¹ø. ·Î¶Ç
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
---
+++ C++/0x0C/0x0C_13.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/1759
+// 1759번. 암호 만들기
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
