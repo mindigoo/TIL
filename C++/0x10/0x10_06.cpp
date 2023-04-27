c6fcd1803799359c92d1d865ff70508549a627a3
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-04-27 22:59:03 +0900

    Add files via upload

---
+++ C++/0x10/0x10_06.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/1003
+// 1003번. 피보나치 함수
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void InputUserData(){
+
+}
+
+void CheckTable(){
+
+}
+
+void OutputResultData(){
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    InputUserData();
+    CheckTable();
+    OutputResultData();
+}
