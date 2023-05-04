bdb4b38922a83f28676a1616ff8530a8878c528f
mindigoo <101114170+mindigoo@users.noreply.github.com>
2023-05-04 20:13:29 +0900

    Add files via upload

--- C++/0x12/0x12_01.cpp
+++ C++/0x12/0x12_01.cpp
@@ -1,5 +1,5 @@
-// https://www.acmicpc.net/problem/11399
-// 11399번. ATM
+// https://www.acmicpc.net/problem/15894
+// 15894. 수학은 체육과목입니다
 
 #include<bits/stdc++.h>
 
@@ -9,5 +9,11 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    long long num;
+    cin >> num;
 
+    cout << (3 * num) + num << "\n";
 }
+
+// 틀린 이유 : 자료형(int -> long long) - 코드 제출 전 최대 큰 입력값 출력해보기
+// 다른 코드 : https://github.com/encrypted-def/basic-algo-lecture/blob/master/0x12/solutions/15894.cpp
